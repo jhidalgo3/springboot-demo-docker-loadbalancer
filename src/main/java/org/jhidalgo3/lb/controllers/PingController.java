4238ba1c6d3fb7db055312266dceb4613f9eac35
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2017-01-05 18:27:23 -0600

    Add @author comment

--- src/main/java/org/jhidalgo3/lb/controllers/PingController.java
+++ src/main/java/org/jhidalgo3/lb/controllers/PingController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * @author jhidalgo3 
+ *
+ */
 
 @RestController
 @RequestMapping("/api/ping")
